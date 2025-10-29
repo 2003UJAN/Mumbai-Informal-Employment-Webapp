2d563add07455e69231a0a6ee5894b7dc39b5578
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-10-29 10:17:57 +0530

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let labels = statsData.map(x => x.area_name);
+    let density = statsData.map(x => x.informal_employment_density);
+
+    let chartData = [{
+        x: labels,
+        y: density,
+        type: "bar"
+    }];
+
+    Plotly.newPlot("chart_overlap", chartData, { title: "Informal Employment Density by Area" });
+
+});
 
